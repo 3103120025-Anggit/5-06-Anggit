8089843372d85952e878e079904b4d97ae0dab83
3103120025-Anggit <3103120025-Anggit@gmail.com>
2022-02-16 12:53:32 +0700

    Praktek3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 
 Route::get('/', function () {
     return view('home', [
@@ -34,3 +33,4 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+Route::resource('/contacts', ContactController::class);
